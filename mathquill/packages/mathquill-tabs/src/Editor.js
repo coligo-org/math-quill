a01ddb24bbdb2d3990d689ed424d48f0a2c29283
Ahmed Mohamadeen <ah.mohamadeen@gmail.com>
2021-07-09 21:33:44 +0200

    WIP: remove non-react demo of the library

--- mathquill/packages/mathquill-tabs/src/Editor.js
+++ mathquill/packages/mathquill-tabs/src/Editor.js
@@ -1,23 +1,23 @@
 // KaTeX dependency
-import katex from "katex";
+import katex from 'katex';
 window.katex = katex;
-import "katex/dist/katex.css";
+import 'katex/dist/katex.css';
 
 // Quill dependency
-import ReactQuill, { Quill } from "react-quill";
-import "react-quill/dist/quill.snow.css";
+import ReactQuill, { Quill } from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 // MathQuill dependency
-import "./jquery";
-import "mathquill/build/mathquill.js";
-import "mathquill/build/mathquill.css";
+import './jquery';
+import 'mathquill/build/mathquill.js';
+import 'mathquill/build/mathquill.css';
 
 // mathquill4quill include
-import mathquill4quill from "mathquill4quill";
-import "mathquill4quill/mathquill4quill.css";
+import mathquill4quill from '../mathquill4quill';
+import '../mathquill4quill.css';
 
 // demo page
-import React from "react";
+import React from 'react';
 
 export default class Editor extends React.Component {
   reactQuill = React.createRef();
@@ -26,7 +26,7 @@ export default class Editor extends React.Component {
     const enableMathQuillFormulaAuthoring = mathquill4quill({ Quill, katex });
     enableMathQuillFormulaAuthoring(
       this.reactQuill.current.editor,
-      this.props.options
+      this.props.options,
     );
   }
 
@@ -37,7 +37,7 @@ export default class Editor extends React.Component {
         id="editor"
         modules={{
           formula: true,
-          toolbar: [["video", "bold", "italic", "underline", "formula"]]
+          toolbar: [['video', 'bold', 'italic', 'underline', 'formula']],
         }}
         placeholder="Type text here, or click on the formula button to enter math."
         theme="snow"
